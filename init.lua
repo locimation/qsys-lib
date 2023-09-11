64251227c6e37ea0dabd7a82615071942654bc1e
BuildTools <unconfigured@null.spigotmc.org>
2023-09-11 23:56:16 +1000

    deep_copy fix

--- init.lua
+++ init.lua
@@ -21,9 +21,9 @@ function deep_copy(orig, copies)
           copy = {}
           copies[orig] = copy
           for orig_key, orig_value in next, orig, nil do
-              copy[deepcopy(orig_key, copies)] = deepcopy(orig_value, copies)
+              copy[deep_copy(orig_key, copies)] = deep_copy(orig_value, copies)
           end
-          setmetatable(copy, deepcopy(getmetatable(orig), copies))
+          setmetatable(copy, deep_copy(getmetatable(orig), copies))
       end
   else -- number, string, boolean, etc
       copy = orig
